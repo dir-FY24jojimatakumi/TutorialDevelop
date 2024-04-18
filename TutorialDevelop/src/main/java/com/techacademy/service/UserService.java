b21859c105469a1ca3594ef94193f26da476a7f2
dir-FY24jojimatakumi <takumi.jojima.y033@dirbato.co.jp>
2024-04-18 10:03:54 +0900

    second

--- TutorialDevelop/src/main/java/com/techacademy/service/UserService.java
+++ TutorialDevelop/src/main/java/com/techacademy/service/UserService.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional; // 追加
 
 import com.techacademy.entity.User;
 import com.techacademy.repository.UserRepository;
-
 @Service
 public class UserService {
     @Autowired
@@ -18,4 +18,17 @@ public class UserService {
         // リポジトリのfindAllメソッドを呼び出す
         return userRepository.findAll();
     }
+ // ----- 追加:ここから -----
+    /** Userを1件検索して返す */
+    public User getUser(Integer id) {
+        return userRepository.findById(id).get();
+    }
+    // ----- 追加:ここまで -----
+    // ----- 追加:ここから -----
+    /** Userの登録を行う */
+    @Transactional
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+    // ----- 追加:ここまで -----
 }
